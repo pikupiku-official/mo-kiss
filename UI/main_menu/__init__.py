52f9fb7e26dee09bc7397aa9d1688429bde690ab
pikupiku-official <pikupiku.official@gmail.com>
2025-06-28 10:00:48 +0900

    Delete UI directory

--- UI/main_menu/__init__.py
+++
@@ -1,5 +0,0 @@
-from .main_menu import MainMenu
-from .main_menu_config import MenuState, COLORS, FONT_SIZES
-from .ui_components import Button, Slider, Panel, VolumeIndicator
-
-__all__ = ['MainMenu', 'MenuState', 'COLORS', 'FONT_SIZES', 'Button', 'Slider', 'Panel', 'VolumeIndicator']
