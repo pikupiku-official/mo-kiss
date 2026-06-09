22d8b9fddbb9fa72182be797874b71f807366675
pikupiku-official <pikupiku.official@gmail.com>
2026-06-09 16:02:23 +0900

    api_config.sample.phpを削除

--- tools/ks-editor/api_config.sample.php
+++
@@ -1,5 +0,0 @@
-<?php
-// このファイルをコピーして api_config.php を作成し、サーバーに置いてください
-// api_config.php は .gitignore で除外済み（リポジトリにコミットしないこと）
-define('GITHUB_PAT', 'ghp_YOUR_TOKEN_HERE');   // GitHub PAT
-define('APP_SECRET',  'YOUR_PASSWORD_HERE');    // index.html の PASSWORD と同じ値
